a487441483dcc3324e2e1001e60115f8ef23d196
burak <rburaksaritas@gmail.com>
2023-07-13 10:25:24 +0300

    Fix build dependencies and usage

--- booklendingapi/src/main/java/com/rburaksaritas/booklendingapi/model/Book.java
+++ booklendingapi/src/main/java/com/rburaksaritas/booklendingapi/model/Book.java
@@ -1,7 +1,7 @@
 package com.rburaksaritas.booklendingapi.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Book {
