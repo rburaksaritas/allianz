d5386048cb169f7e4c91079071cc722fbb52f17b
burak <rburaksaritas@gmail.com>
2023-07-19 16:17:52 +0300

    Initialize OMS API package with Spring Initializr

---
+++ ordermanagementsystem-api/src/main/java/com/rburaksaritas/ordermanagementsystemapi/OrdermanagementsystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.rburaksaritas.ordermanagementsystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermanagementsystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermanagementsystemApiApplication.class, args);
+	}
+
+}
---
+++ ordermanagementsystem-api/src/test/java/com/rburaksaritas/ordermanagementsystemapi/OrdermanagementsystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rburaksaritas.ordermanagementsystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermanagementsystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
