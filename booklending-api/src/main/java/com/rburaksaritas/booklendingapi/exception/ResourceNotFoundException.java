3556d2e354d78a7387dd963a62ae297aff1070c7
burak <rburaksaritas@gmail.com>
2023-07-20 11:55:39 +0300

    Cancel rename

--- booklending-api/src/main/java/com/rburaksaritas/booklendingapi/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.rburaksaritas.booklendingapi.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
-        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
-    }
-}
--- booklending-api/src/main/java/com/rburaksaritas/booklendingapi/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rburaksaritas.booklendingapi.repository;
-
-import com.rburaksaritas.booklendingapi.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
