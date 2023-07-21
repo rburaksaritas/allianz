52fdce98bec5fe363fe3d9dc7538de4256b8a983
burak <rburaksaritas@gmail.com>
2023-07-21 13:24:37 +0300

    Add ProductService unit tests

--- ordermanagementsystem-api/src/test/java/com/rburaksaritas/ordermanagementsystemapi/service/ProductServiceImplTest.java
+++ ordermanagementsystem-api/src/test/java/com/rburaksaritas/ordermanagementsystemapi/service/ProductServiceImplTest.java
@@ -41,76 +41,78 @@ class ProductServiceTests {
         productService = new ProductServiceImpl(productRepository, productModelMapper);
     }
 
-    // CategoryService Tests
+    // ProductService Tests
     @Test
-    public void CategoryService_GetAll_ReturnsAllCategories() {
+    public void ProductService_GetAll_ReturnsAllProducts() {
         // Arrange
-        List<Category> categories = new ArrayList<>();
-        categories.add(new Category(1, "Category 1", "Detail 1", new Date()));
-        categories.add(new Category(2, "Category 2", "Detail 2", new Date()));
-        when(categoryRepository.findAll()).thenReturn(categories);
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Product 1", 10.0, "thumbnail1", "Detail 1", new Category(), 100, new Date()));
+        products.add(new Product(2, "Product 2", 20.0, "thumbnail2", "Detail 2", new Category(), 50, new Date()));
+        when(productRepository.findAll()).thenReturn(products);
 
         // Act
-        List<CategoryDTO> categoryDTOList = categoryService.getAllCategories();
+        List<ProductDTO> productDTOList = productService.getAllProducts();
 
         // Assert
-        assertNotNull(categoryDTOList);
-        assertEquals(categories.size(), categoryDTOList.size());
+        assertNotNull(productDTOList);
+        assertEquals(products.size(), productDTOList.size());
     }
 
     @Test
-    public void CategoryService_GetById_ValidCategoryReturnsCategory() {
+    public void ProductService_GetById_ValidProductReturnsProduct() {
         // Arrange
-        int categoryId = 1;
-        Category category = new Category(1, "Test Category", "Test Detail", new Date());
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(category));
+        int productId = 1;
+        Product product = new Product(1, "Test Product", 10.0, "thumbnail", "Test Detail", new Category(), 100, new Date());
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
 
         // Act
-        CategoryDTO categoryDTO = categoryService.getCategoryById(categoryId);
+        ProductDTO productDTO = productService.getProductById(productId);
 
         // Assert
-        assertNotNull(categoryDTO);
-        assertEquals(category.getId(), categoryDTO.getId());
-        assertEquals(category.getName(), categoryDTO.getName());
+        assertNotNull(productDTO);
+        assertEquals(product.getId(), productDTO.getId());
+        assertEquals(product.getName(), productDTO.getName());
     }
 
     @Test
-    public void CategoryService_GetById_CategoryNotFoundThrowsResourceNotFoundException() {
+    public void ProductService_GetById_ProductNotFoundThrowsResourceNotFoundException() {
         // Arrange
-        int categoryId = 1;
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.empty());
+        int productId = 1;
+        when(productRepository.findById(productId)).thenReturn(Optional.empty());
 
         // Act & Assert
-        assertThrows(ResourceNotFoundException.class, () -> categoryService.getCategoryById(categoryId));
+        assertThrows(ResourceNotFoundException.class, () -> productService.getProductById(productId));
     }
 
     @Test
-    public void CategoryService_SaveCategory_ValidCategoryDTO_ReturnsSavedCategoryDTO() {
+    public void ProductService_SaveProduct_ValidProductDTO_ReturnsSavedProductDTO() {
         // Arrange
-        CategoryDTO categoryDTO = new CategoryDTO();
-        categoryDTO.setName("Test Category");
-        Category category = categoryModelMapper.map(categoryDTO, Category.class);
-        when(categoryRepository.save(category)).thenReturn(category);
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setName("Test Product");
+        productDTO.setPrice(10.0);
+        Product product = productModelMapper.map(productDTO, Product.class);
+        when(productRepository.save(product)).thenReturn(product);
 
         // Act
-        CategoryDTO savedCategoryDTO = categoryService.saveCategory(categoryDTO);
+        ProductDTO savedProductDTO = productService.saveProduct(productDTO);
 
         // Assert
-        assertNotNull(savedCategoryDTO);
-        assertEquals(category.getId(), savedCategoryDTO.getId());
-        assertEquals(category.getName(), savedCategoryDTO.getName());
+        assertNotNull(savedProductDTO);
+        assertEquals(product.getId(), savedProductDTO.getId());
+        assertEquals(product.getName(), savedProductDTO.getName());
     }
 
     @Test
-    public void CategoryService_SaveCategory_DuplicateCategoryNameThrowsIllegalArgumentException() {
+    public void ProductService_SaveProduct_DuplicateProductNameThrowsIllegalArgumentException() {
         // Arrange
-        CategoryDTO categoryDTO = new CategoryDTO();
-        categoryDTO.setName("Test Category");
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setName("Test Product");
+        productDTO.setPrice(10.0);
 
-        when(categoryRepository.save(any())).thenThrow(DataIntegrityViolationException.class);
+        when(productRepository.save(any())).thenThrow(DataIntegrityViolationException.class);
 
         // Act & Assert
-        assertThrows(IllegalArgumentException.class, () -> categoryService.saveCategory(categoryDTO));
+        assertThrows(IllegalArgumentException.class, () -> productService.saveProduct(productDTO));
     }
 
     @Test
@@ -183,6 +185,19 @@ class ProductServiceTests {
     }
 
     @Test
+    public void ProductService_Delete_ValidProductId_DeletesProduct() {
+        // Arrange
+        int productId = 1;
+        Product existingProduct = new Product(productId, "Test Product", 10.0, "thumbnail", "Test Details", new Category(), 100, new Date());
+        when(productRepository.findById(productId)).thenReturn(Optional.of(existingProduct));
+        // Act
+        assertDoesNotThrow(() -> productService.deleteProduct(productId));
+
+        // Assert
+        verify(productRepository, times(1)).deleteById(productId);
+    }
+
+    @Test
     public void ProductService_UpdateProduct_CategoryNotFoundThrowsResourceNotFoundException() {
         // Arrange
         int productId = 1;
@@ -215,37 +230,24 @@ class ProductServiceTests {
     }
 
     @Test
-    public void CategoryService_Delete_ValidCategoryId_DeletesCategory() {
+    public void ProductService_Delete_ProductNotFoundThrowsResourceNotFoundException() {
         // Arrange
-        int categoryId = 1;
-        Category existingCategory = new Category(categoryId, "Test Category", "Test Details", new Date());
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(existingCategory));
-        // Act
-        assertDoesNotThrow(() -> categoryService.deleteCategory(categoryId));
+        int productId = 1;
+        when(productRepository.findById(productId)).thenReturn(Optional.empty());
 
-        // Assert
-        verify(categoryRepository, times(1)).deleteById(categoryId);
+        // Act & Assert
+        assertThrows(ResourceNotFoundException.class, () -> productService.deleteProduct(productId));
     }
 
     @Test
-    public void CategoryService_Delete_CategoryNotFoundThrowsResourceNotFoundException() {
+    public void ProductService_Delete_AssociatedProductThrowsIllegalArgumentException() {
         // Arrange
-        int categoryId = 1;
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.empty());
+        int productId = 1;
+        Product existingProduct = new Product(productId, "Test Product", 10.0, "thumbnail", "Test Details", new Category(), 100, new Date());
+        when(productRepository.findById(productId)).thenReturn(Optional.of(existingProduct));
+        doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(productId);
 
         // Act & Assert
-        assertThrows(ResourceNotFoundException.class, () -> categoryService.deleteCategory(categoryId));
-    }
-
-    @Test
-    public void CategoryService_Delete_AssociatedCategoryThrowsIllegalArgumentException() {
-        // Arrange
-        int categoryId = 1;
-        Category existingCategory = new Category(categoryId, "Test Category", "Test Details", new Date());
-        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(existingCategory));
-        doThrow(DataIntegrityViolationException.class).when(categoryRepository).deleteById(categoryId);
-
-        // Act & Assert
-        assertThrows(IllegalArgumentException.class, () -> categoryService.deleteCategory(categoryId));
+        assertThrows(IllegalArgumentException.class, () -> productService.deleteProduct(productId));
     }
 }
