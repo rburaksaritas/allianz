ee3a5a72686993ed702caa885ece334b4bf88bc8
burak <rburaksaritas@gmail.com>
2023-07-21 16:24:44 +0300

    Implement CustomerService methods

--- ordermanagementsystem-api/src/main/java/com/rburaksaritas/ordermanagementsystemapi/service/CustomerService.java
+++ ordermanagementsystem-api/src/main/java/com/rburaksaritas/ordermanagementsystemapi/service/CustomerService.java
@@ -8,6 +8,9 @@ public interface CustomerService {
     List<CustomerDTO> getAllCustomers();
     CustomerDTO getCustomerById(Integer id);
     CustomerDTO saveCustomer(CustomerDTO customerDTO);
-    CustomerDTO updateCustomer(Integer id, CustomerDTO customerDTO);
+    CustomerDTO updateCustomer(Integer id, String updatedName,
+                               String updatedLocation, String updatedPhone,
+                               String updatedMail, String updatedBirthDate,
+                               String updatedPassword, Double updatedWalletBalance);
     void deleteCustomer(Integer id);
 }
--- ordermanagementsystem-api/src/main/java/com/rburaksaritas/ordermanagementsystemapi/service/CustomerServiceImpl.java
+++ ordermanagementsystem-api/src/main/java/com/rburaksaritas/ordermanagementsystemapi/service/CustomerServiceImpl.java
@@ -1,12 +1,17 @@
 package com.rburaksaritas.ordermanagementsystemapi.service;
 
 import com.rburaksaritas.ordermanagementsystemapi.dto.CustomerDTO;
+import com.rburaksaritas.ordermanagementsystemapi.exception.ResourceNotFoundException;
+import com.rburaksaritas.ordermanagementsystemapi.model.Customer;
 import com.rburaksaritas.ordermanagementsystemapi.repository.CustomerRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
@@ -22,26 +27,57 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public List<CustomerDTO> getAllCustomers() {
-        return null;
+        List<Customer> customers = customerRepository.findAll();
+        return customers.stream()
+                .map(customer -> modelMapper.map(customer, CustomerDTO.class))
+                .collect(Collectors.toList());
     }
 
     @Override
     public CustomerDTO getCustomerById(Integer id) {
-        return null;
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer", "id", id));
+        return modelMapper.map(customer, CustomerDTO.class);
     }
 
     @Override
     public CustomerDTO saveCustomer(CustomerDTO customerDTO) {
-        return null;
+        Customer customer = modelMapper.map(customerDTO, Customer.class);
+        try {
+            Customer savedCustomer = customerRepository.save(customer);
+            return modelMapper.map(savedCustomer, CustomerDTO.class);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Failed to save the customer:" + e.getMessage());
+        }
     }
 
     @Override
-    public CustomerDTO updateCustomer(Integer id, CustomerDTO customerDTO) {
-        return null;
+    public CustomerDTO updateCustomer(Integer id, String updatedName, String updatedLocation,
+                                      String updatedPhone, String updatedMail, String updatedBirthDate,
+                                      String updatedPassword, Double updatedWalletBalance) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer", "id", id));
+
+        try {
+            Customer updatedCustomer = new Customer(id, updatedName, updatedLocation,
+                    updatedPhone, updatedMail, updatedBirthDate, updatedPassword,
+                    updatedWalletBalance, customer.getTimestamp());
+
+            customerRepository.save(updatedCustomer);
+            return modelMapper.map(updatedCustomer, CustomerDTO.class);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Failed to update the customer:" + e.getMessage());
+        }
     }
 
     @Override
     public void deleteCustomer(Integer id) {
-
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer", "id", id));
+        try {
+            customerRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Failed to delete the customer:" + e.getMessage());
+        }
     }
 }
