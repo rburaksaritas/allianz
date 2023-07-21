7b8f72ef71152deb553053efc5ee2a9f559cb9ad
burak <rburaksaritas@gmail.com>
2023-07-21 09:22:37 +0300

    Add admin and manager structures

---
+++ ordermanagementsystem-api/src/main/java/com/rburaksaritas/ordermanagementsystemapi/model/Admin.java
@@ -0,0 +1,33 @@
+package com.rburaksaritas.ordermanagementsystemapi.model;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "username", length = 200, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", length = 200, nullable = false)
+    private String password;
+
+    @Column(name = "timestamp", nullable = false)
+    private Date timestamp;
+
+    @PrePersist
+    protected void onCreate() {
+        this.timestamp = new Date();
+    }
+}
---
+++ ordermanagementsystem-api/src/main/java/com/rburaksaritas/ordermanagementsystemapi/repository/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.rburaksaritas.ordermanagementsystemapi.repository;
+
+import com.rburaksaritas.ordermanagementsystemapi.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
+
+}
