23dd12db4d5a670888d96eb72f41a166f2c9bee5
burak <rburaksaritas@gmail.com>
2023-07-24 17:11:27 +0300

    Add CategoryController

---
+++ ordermanagementsystem-api/src/main/java/com/rburaksaritas/ordermanagementsystemapi/controller/CategoryController.java
@@ -0,0 +1,47 @@
+package com.rburaksaritas.ordermanagementsystemapi.controller;
+
+import com.rburaksaritas.ordermanagementsystemapi.dto.CategoryDTO;
+import com.rburaksaritas.ordermanagementsystemapi.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping("/get")
+    public ResponseEntity<List<CategoryDTO>> getAllCategories(){
+        return null;
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable Integer id){
+        return null;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<CategoryDTO> addCategory(){
+        return null;
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<CategoryDTO> updateCategory(
+            @PathVariable Integer id,
+            @RequestParam String updatedName,
+            @RequestParam String updatedDetails,
+            @RequestParam Date updatedTimestamp){
+        return null;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Integer id){
+        return null;
+    }
+}
