cd0f79a2c3d9187eb64478351a6798fb721bb32c
burak <rburaksaritas@gmail.com>
2023-07-25 17:06:57 +0300

    Implement StaffController methods

--- ordermanagementsystem-api/src/main/java/com/rburaksaritas/ordermanagementsystemapi/controller/StaffController.java
+++ ordermanagementsystem-api/src/main/java/com/rburaksaritas/ordermanagementsystemapi/controller/StaffController.java
@@ -1,6 +1,7 @@
 package com.rburaksaritas.ordermanagementsystemapi.controller;
 
 import com.rburaksaritas.ordermanagementsystemapi.dto.StaffDTO;
+import com.rburaksaritas.ordermanagementsystemapi.exception.ResourceNotFoundException;
 import com.rburaksaritas.ordermanagementsystemapi.service.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,40 +17,60 @@ public class StaffController {
     private final StaffService staffService;
 
     @Autowired
-    public StaffController(StaffService staffService){
+    public StaffController(StaffService staffService) {
         this.staffService = staffService;
     }
-    
-    // TODO: Implement controller methods based on tests
+
     @GetMapping("/get")
     public ResponseEntity<List<StaffDTO>> getAllStaff() {
-        return null;
+        List<StaffDTO> staffList = staffService.getAllStaff();
+        try{
+            return new ResponseEntity<>(staffList, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/get/{id}")
     public ResponseEntity<StaffDTO> getStaffById(@PathVariable Integer id) {
-        return null;
+        StaffDTO staff = staffService.getStaffById(id);
+        if (staff != null) {
+            return new ResponseEntity<>(staff, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping("/add")
     public ResponseEntity<StaffDTO> addStaff(@RequestBody StaffDTO staffDTO) {
-        return null;
+        StaffDTO savedStaff = staffService.saveStaff(staffDTO);
+        return new ResponseEntity<>(savedStaff, HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<StaffDTO> updateStaff(
             @PathVariable Integer id,
-            @RequestParam String updatedName,
-            @RequestParam String updatedPhone,
-            @RequestParam String updatedMail,
-            @RequestParam String updatedPassword,
-            @RequestParam String updatedRole
+            @RequestParam String name,
+            @RequestParam String phone,
+            @RequestParam String mail,
+            @RequestParam String password,
+            @RequestParam String role
     ) {
-        return null;
+        try {
+            StaffDTO updatedStaff = staffService.updateStaff(id, name, phone, mail, password, role);
+            return new ResponseEntity<>(updatedStaff, HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteStaff(@PathVariable Integer id) {
-        return null;
+        try {
+            staffService.deleteStaff(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
