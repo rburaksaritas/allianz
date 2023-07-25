2e119a524ba40c8bd7f69e389a0c4eb10ecb744b
burak <rburaksaritas@gmail.com>
2023-07-25 16:59:59 +0300

    Implement ManagerController methods

--- ordermanagementsystem-api/src/main/java/com/rburaksaritas/ordermanagementsystemapi/controller/ManagerController.java
+++ ordermanagementsystem-api/src/main/java/com/rburaksaritas/ordermanagementsystemapi/controller/ManagerController.java
@@ -1,6 +1,7 @@
 package com.rburaksaritas.ordermanagementsystemapi.controller;
 
 import com.rburaksaritas.ordermanagementsystemapi.model.Manager;
+import com.rburaksaritas.ordermanagementsystemapi.exception.ResourceNotFoundException;
 import com.rburaksaritas.ordermanagementsystemapi.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,24 +17,34 @@ public class ManagerController {
     private final ManagerService managerService;
 
     @Autowired
-    public ManagerController(ManagerService managerService){
+    public ManagerController(ManagerService managerService) {
         this.managerService = managerService;
     }
 
-    // TODO: Implement controller methods based on tests
     @GetMapping("/get")
     public ResponseEntity<List<Manager>> getAllManagers() {
-        return null;
+        List<Manager> managers = managerService.getAllManagers();
+        try {
+            return new ResponseEntity<>(managers, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/get/{id}")
     public ResponseEntity<Manager> getManagerById(@PathVariable Integer id) {
-        return null;
+        Manager manager = managerService.getManagerById(id);
+        if (manager != null) {
+            return new ResponseEntity<>(manager, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping("/add")
     public ResponseEntity<Manager> addManager(@RequestBody Manager manager) {
-        return null;
+        Manager savedManager = managerService.saveManager(manager);
+        return new ResponseEntity<>(savedManager, HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{id}")
@@ -42,11 +53,21 @@ public class ManagerController {
             @RequestParam String updatedUsername,
             @RequestParam String updatedPassword
     ) {
-        return null;
+        try {
+            Manager updatedManager = managerService.updateManager(id, updatedUsername, updatedPassword);
+            return new ResponseEntity<>(updatedManager, HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteManager(@PathVariable Integer id) {
-        return null;
+        try {
+            managerService.deleteManager(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
